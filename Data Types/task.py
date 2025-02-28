37eaf7368d40120f20de275559e6f91ff9f464f0
devbenpiacenti <devbenpiacenti@gmail.com>
2025-02-28 12:20:07 -0500

    finished project

---
+++ Data Types/task.py
@@ -0,0 +1,8 @@
+print("akjsdhasjdklavshkdjvghaksjhdkavjshdkajhsdv"[-1])
+
+# Integer = whole number
+print(100 + 200)
+
+print(3.14159)
+print(True)
+print(False)
---
+++ Number Manipulation/task.py
@@ -0,0 +1,16 @@
+# # # bmi = 84 / 1.65 ** 2
+# # # print(round(bmi, 2))
+# #
+# # score = 0
+# # score += 1
+# #
+# # print(score)
+#
+# score = 0
+# height = 1.8
+# is_winning = True
+#
+# print(f"Your score is = {score}, your height is {height}. You are winning is {is_winning}")
+
+name = input("What is your name?")
+print(f"Hello, {name}")
